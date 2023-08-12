bbb53af9a116c75c92ace7e991da04c443c42451
SteveMuiyuro <stevemuiyuro@gmail.com>
2023-08-12 20:49:24 +0300

    Create Home page for the Portfolio site

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
