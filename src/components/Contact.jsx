51b345373bf1d4f38a3de33b69f73907b975552a
SteveMuiyuro <stevemuiyuro@gmail.com>
2023-08-17 20:59:16 +0300

    Adjust Margins

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white "
+      className="w-full h-full bg-gradient-to-b from-black to-gray-800 p-4 text-white "
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full mt-80">
         <div className="pb-8">
