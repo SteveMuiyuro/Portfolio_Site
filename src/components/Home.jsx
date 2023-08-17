b7df0f4b2f94a687a0333f71c98919d9fc812059
SteveMuiyuro <stevemuiyuro@gmail.com>
2023-08-17 22:30:34 +0300

    Update header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,8 @@ export default function Home() {
       className="h-screen w-full bg-gradient-to-b from from-black via-black to-gray-800"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-between h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+        <div className="flex flex-col justify-center h-full ">
+          <h2 className="text-3xl sm:text-7xl font-bold text-white m pt-20">
             I'm a Front End Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
